115ffe02d2c1dbc0ca4050b1403114f7e29ce7b9
Pintop <ap.gwhere@gmail.com>
2014-07-17 20:50:18 -0500

    Initial commit of long-sidebar and no-title

---
+++ long-sidebar/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+if ( is_admin() )
+	return;
+
+if ( ! class_exists( 'ExtensionLongSidebar' ) ) {
+	class ExtensionLongSidebar {
+		function ExtensionLongSidebar() {
+			require_once( dirname( __FILE__ ) . '/lib/content-widgets.php' );
+			it_classes_load( 'it-file-utility.php' );
+			$this->_base_url = ITFileUtility::get_url_from_file( dirname( __FILE__ ) );
+			add_action( 'builder_layout_engine_render', array( &$this, 'change_render_content' ), 0 );	
+		}
+		function extension_render_content() {	
+		?>
+			<?php if ( have_posts() ) : ?>
+				<div class="loop">
+					<div class="loop-content">
+						<?php while ( have_posts() ) : // The Loop ?>
+							<?php the_post(); ?>
+							<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<div class="entry-content clearfix">
+									<?php the_content(); ?>
+								</div>
+							</div>								
+						<?php endwhile;  ?>
+					</div>
+				</div>
+			<?php else :  ?>
+				<?php do_action( 'builder_template_show_not_found' ); ?>
+			<?php endif;  ?>
+		<?php
+
+		}
+		function change_render_content() {
+			remove_action( 'builder_layout_engine_render_content', 'render_content' );
+			add_action( 'builder_layout_engine_render_content', array( &$this, 'extension_render_content' ) );
+		}
+	} // end class 
+
+	$ExtensionLongSidebar = new ExtensionLongSidebar();
+}
---
+++ long-sidebar/lib/content-widgets.php
@@ -0,0 +1,80 @@
+<?php
+function register_content_sidebars() {
+		$sidebar = array(
+                'id' => 'above_content_sidebar',
+                'name' => __('Above Content'),
+                'description' => __( 'This widget section sits above the content area' ),
+                'before_widget' => '<div class=""><div class=""><div id="%1$s" class="widget %2$s">',
+                'after_widget' => '</div></div></div>',
+                'before_title' => '<h4 class="widget-title">',
+                'after_title' => '</h4>'
+        );
+
+ 		register_sidebar($sidebar);
+        $sidebar = array(
+                'id' => 'below_content_left_sidebar',
+                'name' => __('Below Content Left Sidebar'),
+                'description' => __( 'This widget section sits below the content area on the left' ),
+                'before_widget' => '<div class="pin-left"><div class="left clearfix"><div id="%1$s" class="widget %2$s">',
+                'after_widget' => '</div></div></div>',
+                'before_title' => '<h4 class="widget-title">',
+                'after_title' => '</h4>'
+        );
+
+ 		register_sidebar($sidebar);
+
+        $sidebar = array(
+                'id' => 'below_content_right_sidebar',
+                'name' => __('Below Content Right Sidebar'),
+                'description' => __( 'This widget section sits below the content area on the right' ),
+                'before_widget' => '<div class="pin-right"><div class="right clearfix"><div id="%1$s" class="widget %2$s">',
+                'after_widget' => '</div></div></div>',
+                'before_title' => '<h4 class="widget-title">',
+                'after_title' => '</h4>'
+        );
+
+        register_sidebar($sidebar);
+}
+add_action('widgets_init', 'register_content_sidebars');
+
+// add the widget area ONLY if there is any content in that widget area
+if ( is_active_sidebar( 'above_content_sidebar' ) ) {
+        add_action( 'builder_module_render_element_block_contents_content', 'insert_above_content_sidebar');   // add sidebar
+}
+
+
+// add the widget area ONLY if there is any content in that widget area
+if ( is_active_sidebar( 'below_content_left_sidebar' ) ) {
+        add_action( 'builder_module_render_element_block_contents_content', 'insert_below_content_left_sidebar', 98);   // add sidebar
+}
+
+// add the widget area ONLY if there is any content in that widget area
+if ( is_active_sidebar( 'below_content_right_sidebar' ) ) {
+        add_action( 'builder_module_render_element_block_contents_content', 'insert_below_content_right_sidebar', 99 );
+}
+
+
+// display the top widget area
+function insert_above_content_sidebar($fields) {
+	
+	dynamic_sidebar( 'above_content_sidebar' );
+               
+
+        
+}
+
+
+
+// display the top widget area
+function insert_below_content_left_sidebar($fields) {
+	
+		dynamic_sidebar( 'below_content_left_sidebar' );
+              
+}
+
+// display the bottom widget area
+function insert_below_content_right_sidebar($fields) {
+        dynamic_sidebar( 'below_content_right_sidebar' );
+        
+}
+
